8994eaf0f10b45584a6db9dc2da165bbc1f3e0f8
Tom Wilderspin <tomwilderspin@gmail.com>
2015-08-18 08:03:00 +0000

    started on tweet filtering

--- livehourFeed/liveFeedStream.js
+++ livehourFeed/liveFeedStream.js
@@ -1,4 +1,24 @@
 
-function broadcastTwitterDataToSockets(filterTwitterData) {
+function getFilteredStream(filterTweetData) {
+    return function(tweet) {
+        return filterTweetData(tweet);
+    };
+}
+
+
+function filterTweetData() {
+    return function(rawTweet) {
+        return {
+            text: rawTweet.text,
+            isRetweet:'',
+            userImage:'',
+            images:[{},{}],
+            userName: '',
+            userHandle:''
+        }
+    }
+}
+
+module.exports = function(TwitterStream, SocketConnection) {
     
 }
